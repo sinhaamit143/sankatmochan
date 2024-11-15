e9c799d2ac6fc900172a3878b3074070fef42f06
Mohit Kumar <mohitvishkarma50@gmail.com>
2024-11-15 19:38:13 +0530

    admin dashboard images

--- admin/src/app/dashboard/dashboard-components/blog-cards/blog-cards.component.ts
+++ admin/src/app/dashboard/dashboard-components/blog-cards/blog-cards.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
 import { AlertService } from 'src/app/services/alert.service';
+import { FileService } from 'src/app/services/file.service';
+import { environment } from 'src/environments/environment';
+import { HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-blog-cards',
@@ -12,12 +15,16 @@ export class BlogCardsComponent implements OnInit {
   p: number = 1; 
   itemsPerPage: number = 4; 
   getData:any
+  env: any;
+  image: File[];
+  selectedBlog: any = {};
 
   constructor(
-    private api : ApiService,
-    private as : AlertService
+    private _blogService : ApiService,
+    private as : AlertService,
+    private fileServ : FileService
   ) {
-
+    this.env = environment.url;
     for (let i = 1; i <= 100; i++) {
       this.collection.push(`item ${i}`);
     }
@@ -28,13 +35,36 @@ export class BlogCardsComponent implements OnInit {
   }
 
   getBlogs() {
-    this.api.get('blogs', {}).subscribe((res: any) => {
-      if (res) {
-        this.getData = res
-      } else {
-        this.as.warningToast(res.error.message)
-      }
-    })
+    this._blogService.get('blogs',{}).subscribe(res => {
+      this.getData = res;
+    });
   }
 
+  handleFileInput(event: any) {
+    this.selectedBlog.image = event.target.files[0]; 
+  }
+
+  uploadImage(modal:any) {
+    if (this.image.length > 0) {
+      this.fileServ.uploadFile(this.image[0]).subscribe(
+        (res: any) => {
+          if (res.type === HttpEventType.Response) {
+            const body: any = res.body;
+            const imagePath = body.file.path;
+            this.selectedBlog.image = imagePath;
+            console.log('blog', this.selectedBlog)
+            this._blogService.put('blogs',this.selectedBlog._id, this.selectedBlog).subscribe(res => {
+              this.getBlogs();
+              modal.close(); 
+            });
+          }
+        },
+        (error) => {
+          alert(`Error uploading image: ${error.message}`);
+        }
+      );
+    } else {
+      alert('Please Select Image');
+    }
+  }
 }
